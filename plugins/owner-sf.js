ccfb2a38714f093661fe0d60c9e8a6a410276fdc
KHALID NICCUR <113862545+Khalid-niccur@users.noreply.github.com>
2023-07-12 21:14:40 +0300

    Update owner-sf.js

--- plugins/owner-sf.js
+++ plugins/owner-sf.js
@@ -1,14 +1,29 @@
-import fs from 'fs'
-let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) throw `uhm.. where is the text?\n\nusage:\n${usedPrefix + command} <teks>\n\nexample:\n${usedPrefix + command} plugins/melcanz.js`
-    if (!m.quoted.text) throw `balas pesan nya!`
-    let path = `${text}`
-    await fs.writeFileSync(path, m.quoted.text)
-    m.reply(`stored in ${path}`)
-}
-handler.help = ['sf'].map(v => v + ' <teks>')
-handler.tags = ['owner']
-handler.command = /^sf$/i
-
-handler.rowner = true
-export default handler
+import { tmpdir } from 'os' 
+ import path, { join } from 'path' 
+ import { 
+   readdirSync, 
+   statSync, 
+   unlinkSync, 
+   existsSync, 
+   readFileSync, 
+   writeFileSync,
+   watch 
+ } from 'fs' 
+ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, text }) => { 
+  
+ let ar = Object.keys(plugins) 
+     let ar1 = ar.map(v => v.replace('.js', '')) 
+     if (!text) throw `uhm.. where the text?\n\nexample:\n${usedPrefix + command} info` 
+     if (!ar1.includes(args[0])) return m.reply(`*🗃 NOT FOUND!*\n==================================\n\n${ar1.map(v => ' ' + v).join`\n`}`) 
+ const path = join(__dirname, '../plugins/' + args[0] + '.js') 
+     await fs.writeFileSync(path, m.quoted.text)
+ conn.reply(m.chat, `Succes adding "plugins/${args[0]}.js"`, m) 
+  
+ } 
+ handler.help = ['sf'] 
+ handler.tags = ['owner'] 
+ handler.command = /^(sf)$/i 
+  
+ handler.mods = true 
+  
+ export default handler
