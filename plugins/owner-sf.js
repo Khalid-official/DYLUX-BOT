efdc163df74e9045f644c8b63af6042fd2aec7e9
KHALID NICCUR <113862545+Khalid-niccur@users.noreply.github.com>
2023-07-13 08:58:07 +0300

    Update owner-sf.js

--- plugins/owner-sf.js
+++ plugins/owner-sf.js
@@ -14,7 +14,6 @@ import { tmpdir } from 'os'
  let ar = Object.keys(plugins) 
      let ar1 = ar.map(v => v.replace('.js', '')) 
      if (!text) throw `uhm.. where the text?\n\nexample:\n${usedPrefix + command} info` 
-     if (!ar1.includes(args[0])) return m.reply(`*🗃 NOT FOUND!*\n==================================\n\n${ar1.map(v => ' ' + v).join`\n`}`) 
  const path = join(__dirname, '../plugins/' + args[0] + '.js') 
      await fs.writeFileSync(path, m.quoted.text)
  conn.reply(m.chat, `Succes adding "plugins/${args[0]}.js"`, m) 
