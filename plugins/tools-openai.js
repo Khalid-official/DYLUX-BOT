cdc6708f51f08eb97db058afd290e0a149509a77
KHALID NICCUR <113862545+Khalid-niccur@users.noreply.github.com>
2023-07-17 17:21:17 +0300

    Update tools-openai.js

--- plugins/tools-openai.js
+++ plugins/tools-openai.js
@@ -1,18 +1,17 @@
-
 import cheerio from 'cheerio'
 import gpt from 'api-dylux'
-let handler = async (m, { conn, text }) => {
-	
-if (!text) throw `✳️ Ingresa el texto`
+let handler = async(m, { conn, text }) => {
+
+if (!text) throw `✳✨🎈Enter the text🎈✨`
 m.react('💬')
 
-	try {
-        let syms = `Eres DyLux Bot, un gran modelo de lenguaje entrenado por OpenAI. Siga cuidadosamente las instrucciones del usuario. Responde usando Markdown.`
-        let res = await gpt.ChatGpt(text, syms)
-         await m.reply(res.text)
-	} catch {
-		m.reply(`❎ Error: intenta más tarde`)
-	}
+try {
+         let syms = `You are DyLux Bot, a great language model trained by OpenAI. Carefully follow the user instructions. Respond using Markdown.`
+         let res = await gpt.ChatGpt(text, syms)
+          await m.reply(res.text)
+} catch {
+m.reply(`❎ Error: try again later`)
+}
 
 }
 handler.help = ['ia <text>']
