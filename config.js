d1b94095c8709469f8a23fe9abd6edece07e3c76
KHALID NICCUR <113862545+Khalid-niccur@users.noreply.github.com>
2023-07-27 21:42:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5492622271736'], 
-  ['5492622271736'] 
+  ['254736958034', 'KHALID-TECHNOLOGIES', true],
+  ['923470027813'], 
+  ['254793618247'] 
 ] //Numeros de owner 
 
-global.mods = ['573125484672'] 
-global.prems = ['50489079501', '5219631769130', '573125484672']
+global.mods = ['254736958034'] 
+global.prems = ['254736958034', '254793618247', '923470027813']
 global.APIs = { // API Prefix
   // name: 'https://website'
   xteam: 'https://api.xteam.xyz', 
@@ -26,15 +26,15 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'DyLux┃ᴮᴼᵀ' 
-global.author = '@fg98' 
+global.author = 'KHALID-tech' 
 global.fgig = '▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff\n' 
-global.dygp = 'https://chat.whatsapp.com/IO9jmpI72ejHiN4unRZleU'
-global.fgsc = 'https://github.com/FG98F/dylux-fg' 
-global.fgyt = 'https://youtube.com/fg98f'
+global.dygp = 'https://github.com/Khalid-niccur'
+global.fgsc = 'https://github.com/Khalid-niccur' 
+global.fgyt = 'https://github.com/Khalid-niccur'
 global.fgpyp = 'https://paypal.me/fg98f'
 global.fglog = 'https://i.imgur.com/Owmb93c.png' 
 
-global.wait = '*⌛ _Cargando..._*\n*▰▰▰▱▱▱▱▱*'
+global.wait = '💓𝕡𝕝𝕖𝕒𝕤𝕖 𝕒𝕨𝕒𝕚𝕥 𝕔𝕠𝕞𝕣𝕒𝕕𝕖⌛ \n*▰▰▰▱▱▱▱▱*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✅'
