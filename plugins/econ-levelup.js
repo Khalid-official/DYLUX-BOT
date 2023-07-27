3bbbe2af7de570665bbc62bf30feaab307f19ee0
KHALID NICCUR <113862545+Khalid-niccur@users.noreply.github.com>
2023-07-27 22:16:58 +0300

    Update econ-levelup.js

--- plugins/econ-levelup.js
+++ plugins/econ-levelup.js
@@ -1,42 +1,42 @@
+
 import { canLevelUp, xpRange } from '../lib/levelling.js'
-let handler = async(m, { conn }) => {
-let name = conn.getName(m.sender)
-     let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/bf7a9b9b07b47020c88e0.jpg')
-     let user = global.db.data.users[m.sender]
-     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
-         let { min, xp, max } = xpRange(user.level, global.multiplier)
-         let txt = `
+let handler = async (m, { conn }) => {
+	  let name = conn.getName(m.sender)
+    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.ibb.co/1ZxrXKJ/avatar-contact.jpg')
+    let user = global.db.data.users[m.sender]
+    if (!canLevelUp(user.level, user.exp, global.multiplier)) {
+        let { min, xp, max } = xpRange(user.level, global.multiplier)
+let txt = `
 ┌───⊷ *LEVEL*
 ▢ Name : *${name}*
 ▢ Level : *${user.level}*
 ▢ XP : *${user.exp - min}/${xp}*
 ▢ Rank : *${user.role}*
 └──────────────
-
-You need *${max - user.exp}* of *XP* to level up
+Te falta *${max - user.exp}* de *XP* para subir de nivel
 `.trim()
 try {
-   let imgg = API('fgmods', '/api/maker/rank', {
-     username: name,
-     xp: user.exp -min,
-     exp: xp,
-     avatar: pp,
-     level: user.level,
-     ranklog: 'https://i.ibb.co/7gfnyMw/gold.png',
-     background: 'https://i.ibb.co/CsNgBYw/qiyana.jpg'
+  let imgg = API('fgmods', '/api/maker/rank', {
+    username: name,
+    xp: user.exp - min,
+    exp: xp,
+    avatar: pp,
+    level: user.level,
+    ranklog: 'https://i.ibb.co/7gfnyMw/gold.png',
+    background: 'https://i.ibb.co/CsNgBYw/qiyana.jpg'
 }, 'apikey')
 
-     conn.sendFile(m.chat, imgg, 'level.jpg', txt, m)
-} catch(e) {
-     m.reply(txt)
+    conn.sendFile(m.chat, imgg, 'level.jpg', txt, m)
+} catch (e) {
+    m.reply(txt)
 }
-     }
-     let before = user.level * 1
-     while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-     if (before !== user.level) {
-     user.role = global.rpg.role(user.level).name
+    }
+    let before = user.level * 1
+    while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+    if (before !== user.level) {
+    	user.role = global.rpg.role(user.level).name
 
-         let str = `
+        let str = `
 ┌─⊷ *LEVEL UP*
 ▢ Previous level : *${before}*
 ▢ Current level : *${user.level}*
@@ -45,19 +45,19 @@ try {
 
 *_The more you interact with the bots, the higher your level will be_*
 `.trim()
-         try {
-             let img = API('fgmods', '/api/maker/levelup', {
-                 avatar: pp
-              }, 'apikey')
-       conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
-         } catch(e) {
-             m.reply(str)
-         }
-     }
+        try {
+            let img = API('fgmods', '/api/maker/levelup', { 
+                avatar: pp 
+             }, 'apikey')
+      conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
+        } catch (e) {
+            m.reply(str)
+        }
+    }
 }
 
 handler.help = ['levelup']
 handler.tags = ['econ']
-handler.command = ['level', 'lvl', 'levelup', 'level']
+handler.command = ['nivel', 'lvl', 'levelup', 'level'] 
 
 export default handler
