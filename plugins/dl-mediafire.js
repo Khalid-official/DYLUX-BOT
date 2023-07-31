7d63875967fc794cbc7cd11eb0118e6096bc3dd7
KHALID NICCUR <113862545+Khalid-niccur@users.noreply.github.com>
2023-07-31 18:59:14 +0300

    Update dl-mediafire.js

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -1,62 +1,19 @@
-
-import fetch from 'node-fetch'
 import { mediafiredl } from '@bochilteam/scraper'
-import fg from 'api-dylux'
-let free = 150 // limite de descarga
-let prem = 300 //si su servidor tienes menos de 2GB baja el límite
-let handler = async (m, { conn, args, text, usedPrefix, command, isOwner, isPrems }) => {
-	
-   if (!args[0]) throw `✳️Enter the mediafire link next to the command`
-    if (!args[0].match(/mediafire/gi)) throw `💚Link incorincorrect 💛`
-    m.react(rwait)
-    
-    let limit = isPrems || isOwner ? prem : free
-	let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
-    let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url: u }))).buffer()
-    try {
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
     let res = await mediafiredl(args[0])
     let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
-    let isLimit = limit * 1024 < filesize
     let caption = `
-  ≡ *MEDIAFIRE*
-▢ *Name:* ${filename}
-▢ *Size:* ${filesizeH}
-▢ *Extension:* ${ext}
-▢ *Uploaded:* ${aploud}
-${isLimit ? `\n▢ File exceeds download limit *+${free} MB*\nGo premium to be able to download files larger than *${prem} MB*` : ''}
+*💌 Name:* ${filename}
+*📊 Size:* ${filesizeH}
+*🗂️ Extension:* ${ext}
+*📨 Uploaded:* ${aploud}
 `.trim()
-    await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m)  
-    if(!isLimit) await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
-    m.react(done)
-    
-    } catch {
-
-        try {
-	let res = await fg.mediafireDl(args[0])
-     let { url, url2, filename, ext, upload_date, filesize, filesizeB } = res
-    let isLimit = limit * 1024 < filesizeB
-   letcaption = `
-    ≡ *MEDIAFIRE*
-▢ *Name:* ${filename}
-▢ *Size:* ${filesize}
-▢ *Extension:* ${ext}
-▢ *Uploaded:* ${upload_date}
-${isLimit ? `\n▢ File exceeds download limit *+${free} MB*\nGo premium to be able to download files larger than *${prem} MB*` : ''}
-`.trim()
-await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m)
-if(!isLimit) await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
-    m.react(done)
-} catch {
-     m.reply(`Error: try another link`)
+    m.reply(caption)
+    await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
 }
-
-  }
-  
-}
-handler.help = ['mediafire <url>']
-handler.tags = ['dl', 'prem']
-handler.command = ['mediafire', 'mfire'] 
-handler.diamond = true
-handler.premium = false
+handler.help = ['mediafire'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(mediafire|mf)$/i
 
 export default handler
