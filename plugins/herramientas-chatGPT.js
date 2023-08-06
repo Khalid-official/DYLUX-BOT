da3daf22d87537f85bc5657b60fc1a3c80725889
KHALID NICCUR <113862545+Khalid-niccur@users.noreply.github.com>
2023-08-07 00:41:30 +0300

    Update herramientas-chatGPT.js

--- plugins/herramientas-chatGPT.js
+++ plugins/herramientas-chatGPT.js
@@ -1,30 +1,28 @@
-import fetch from 'node-fetch'
-import cheerio from 'cheerio'
-import gpt from 'api-dylux'
+import fetch from 'node-fetch';
+
 let handler = async (m, { text, usedPrefix, command }) => {
-if (!text) throw `*[❗] enter a request a command to use the CHATGPT function*\n\n*—◉ Examples of command requests*\n*◉ ${usedPrefix + command} Reflection on the Merlina series 2022 from netflix*\n*◉ ${usedPrefix + command} JS code for a card game*`
-try {
-await conn.sendPresenceUpdate('composing', m.chat)
-let syms = `You will act as a WhatsApp Bot which was created by BrunoSobrino, you will be The Mystic - Bot.`
-let res = await gpt.ChatGpt(text, syms)
-await m.reply(res.text)
-} catch {
-try {
-let IA2 = await fetch(`https://api.amosayomide05.cf/gpt/?question=${text}&string_id=${m.sender}`)
-let IAR2 = await IA2.json()
-m.reply(`${IAR2.response}`.trim())
-} catch {
-try {
-let rrEes = await fetch(`https://api.ibeng.tech/api/info/openai?text=${text}&apikey=tamvan`)
-let jjJson = await rrEes.json()
-m.reply(jjJson.data.data.trim())
-} catch {
-try {
-let tioress = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=BrunoNephew&text=${text}&user=${m.sender}`)
-let hasill = await tioress.json()
-m.reply(`${hasill.result}`.trim())
-} catch {
-throw `*[❗] error , try again*`
-}}}}}
-handler.command = ['openai', 'chatgpt', 'ia', 'robot']
-export default handler
+  
+  if (!text && !(m.quoted && m.quoted.text)) {
+    throw `Please provide some text or quote a message to get a response.`;
+  }
+
+ 
+  if (!text && m.quoted && m.quoted.text) {
+    text = m.quoted.text;
+  }
+
+  try {
+    const response = await fetch(`https://guru-gpt4-prod-gpt4-reverse-o8hyfh.mo1.mogenius.io/api/gpt4?query=${encodeURIComponent(text)}`);
+    const data = await response.json();
+    const { response: result } = data; 
+    m.reply(result.trim()); 
+  } catch (error) {
+    console.error('Error:', error); 
+    throw `*ERROR*`;
+  }
+};
+
+handler.command = ['bro', 'chatgpt', 'ai', 'siri'];
+handler.diamond = false;
+
+export default handler;
