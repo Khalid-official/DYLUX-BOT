b81835e6ae92d19d07f20c78cacfe1f0785ca0ac
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2024-04-26 21:49:27 +0300

    Update sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,17 +1,64 @@
+import fetch from 'node-fetch'
+import { sticker, addExif } from '../lib/sticker.js'
+import { Sticker } from 'wa-sticker-formatter'
+let handler = async(m, { conn, text, args, usedPrefix, command }) => {
+if (!text) throw `*[❗] 𝐞𝐧𝐭𝐞𝐫 𝐭𝐞𝐱𝐭*\n\n*—◉ EXAMPLE:*\n*◉ ${usedPrefix + command} Dylux Bot*`
+let teks = encodeURI(text)
 
-import { sticker } from '../lib/sticker.js'
-import fg from 'api-dylux'
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (command == 'attp') {
+let a1 = await (await fetch(`https://api.erdwpe.com/api/maker/attp?text=${teks}`)).buffer()
+let a2 = await createSticker(a1, false, global.packname, global.author)
+conn.sendFile(m.chat, a2, 'sticker.webp', '', m, { asSticker: true })}
     
-    if (!text) throw `✳️ Envie el texto\n\n📌Ejemplo *${usedPrefix + command}* dylux-fg`  
-    let color = '2FFF2E' //color
-    let res = await fg.ttp(text, color) 
-    let stiker = await sticker(null, res.result, global.packname, global.author)
-    if (stiker) return await conn.sendFile(m.chat, stiker, '', '', m, null, rpl)
-    throw stiker.toString()
 }
-handler.help = ['ttp <text>']
+handler.command = handler.help = ['attp']
 handler.tags = ['sticker']
-handler.command = ['ttp']
-
 export default handler
+
+async function createSticker(img, url, packName, authorName, quality) {
+let stickerMetadata = { type: 'full', pack: packName, author: authorName, quality }
+return (new Sticker(img ? img : url, stickerMetadata)).toBuffer()}
+async function mp4ToWebp(file, stickerMetadata) {
+if (stickerMetadata) {
+if (!stickerMetadata.pack) stickerMetadata.pack = '‎'
+if (!stickerMetadata.author) stickerMetadata.author = '‎'
+if (!stickerMetadata.crop) stickerMetadata.crop = false
+} else if (!stickerMetadata) { stickerMetadata = { pack: '‎', author: '‎', crop: false }}
+let getBase64 = file.toString('base64')
+const Format = { file: `data:video/mp4;base64,${getBase64}`, processOptions: { crop: stickerMetadata?.crop, startTime: '00:00:00.0', endTime: '00:00:7.0', loop: 0
+}, stickerMetadata: { ...stickerMetadata },
+sessionInfo: {
+WA_VERSION: '2.2106.5',
+PAGE_UA: 'WhatsApp/2.2037.6 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
+WA_AUTOMATE_VERSION: '3.6.10 UPDATE AVAILABLE: 3.6.11',
+BROWSER_VERSION: 'HeadlessChrome/88.0.4324.190',
+OS: 'Windows Server 2016',
+START_TS: 1614310326309,
+NUM: '6247',
+LAUNCH_TIME_MS: 7934,
+PHONE_VERSION: '2.20.205.16'
+},
+config: {
+sessionId: 'session',
+headless: true,
+qrTimeout: 20,
+authTimeout: 0,
+cacheEnabled: false,
+useChrome: true,
+killProcessOnBrowserClose: true,
+throwErrorOnTosBlock: false,
+chromiumArgs: [
+'--no-sandbox',
+'--disable-setuid-sandbox',
+'--aggressive-cache-discard',
+'--disable-cache',
+'--disable-application-cache',
+'--disable-offline-load-stale-cache',
+'--disk-cache-size=0'
+],
+executablePath: 'C:\\\\Program Files (x86)\\\\Google\\\\Chrome\\\\Application\\\\chrome.exe',
+skipBrokenMethodsCheck: true,
+stickerServerEndpoint: true
+}}
+let res = await fetch('https://sticker-api.openwa.dev/convertMp4BufferToWebpDataUrl', { method: 'post', headers: { Accept: 'application/json, text/plain, /', 'Content-Type': 'application/json;charset=utf-8', }, body: JSON.stringify(Format)})
+return Buffer.from((await res.text()).split(';base64,')[1], 'base64')}
